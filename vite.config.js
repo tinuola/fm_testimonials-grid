54251749987755c40054692efb5cbc56dd6c3c18
tinuola <24995224+tinuola@users.noreply.github.com>
2023-03-17 04:36:36 -0400

    Cleaning up

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // root: "/src",
-  // base: "/",
-  // build: {
-  //   outDir: "/dist",
-  // },
-  // publicDir: "/public",
 });
