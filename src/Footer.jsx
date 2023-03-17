f9b85b7fa25c54552be337bf4728f0b333d7e3bc
tinuola <24995224+tinuola@users.noreply.github.com>
2023-03-16 23:41:52 -0400

    Adds Footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer className="fm-byline-row">
+      <p className="fm-byline">
+        Challenge by&nbsp;
+        <a
+          href="https://www.frontendmentor.io/challenges/testimonials-grid-section-Nnw6J7Un7"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+      </p>
+      <p className="fm-byline">
+        Coded by&nbsp;
+        <a
+          href="https://github.com/tinuola/fm_testimonials-grid"
+          target="_blank"
+          rel="noreferrer"
+        >
+          TechTinu
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
