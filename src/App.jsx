e6b1c26934ebf6532474c596aecb9af542eee24c
tinuola <24995224+tinuola@users.noreply.github.com>
2023-03-16 23:13:08 -0400

    Create Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Card from "./Card";
+import Data from "./data.json";
 
 const App = () => {
-  return <div>Hello React World!</div>;
+  // console.log(Data);
+  return (
+    <main className="container">
+      <section>
+        <h1 className="sr-only">
+          Testimonials Grid - A Frontend Mentor Project
+        </h1>
+        {Data.map((item) => {
+          const { id, name, image, blurb, testimonial } = item;
+          return (
+            <Card
+              key={id}
+              name={name}
+              image={image}
+              blurb={blurb}
+              testimonial={testimonial}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
